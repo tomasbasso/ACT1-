795075fc0cf4dfddc3e08538154abf81dc7d5408
Tomás Basso Fernández <tomas.basso@hotmail.com>
2022-11-10 20:24:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ACT1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arreglodias={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+            
+            for (int i=0; i<7; i++){
+                Console.WriteLine(arreglodias[i]);
+            }
+        }
+    }
+}
